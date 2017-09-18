444f83f1165179327c9615ce5af390040e036e96
Angela Lin <tigclaw@gmail.com>
2017-09-18 13:59:15 -0400

    Begin board outline and js setup

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('test');
+let board = document.querySelector('#board');
+
+// EVENT LISTENERS
+board.addEventListener('click', function(e) {
+  // console.log(e.target);
+  let target = e.target;
+  let position = target.getAttribute('data-pos');
+  console.log('position is', position);
+
+});
+
+// VARIABLES
+let currentPlayer;
+
+
+
+// METHODS
+function startGame() {
+  let gameInfo = document.querySelector('#game-info');
+  gameInfo.textContent = 'Player 1 start';
+}
+
+function nextTurn() {
+
+}
+
+function clearBoard() {
+
+}
+
+function markX() {
+
+}
+
+function markO() {
+
+}
+
+// RUN UPON PAGE LOAD
+// start game
+startGame();
+
